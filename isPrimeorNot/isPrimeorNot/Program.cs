750a8de48327093f8922e6455d361697513d1836
Ana Marija <47063623+AneAniks@users.noreply.github.com>
2019-11-04 20:17:59 +0100

    Prime

---
+++ isPrimeorNot/isPrimeorNot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace isPrimeOrNot
+{
+    class Program
+    {
+        public static bool isPrimeOrNot(int num)
+        {
+            for( int i=2; i < num; i++ )
+            {
+                if ( num % i == 0 )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string input;
+            int parseNum;
+
+            Console.Write("Enter number to check if its prime or not: ");
+
+            input = Console.ReadLine();
+            parseNum = int.Parse(input);
+            
+            if ( isPrimeOrNot(parseNum) )
+            {
+                Console.WriteLine(parseNum + " is Prime number.");
+            }
+            else
+            {
+                Console.WriteLine(parseNum + " is not Prime number.");
+            }
+        }
+    }
+}
