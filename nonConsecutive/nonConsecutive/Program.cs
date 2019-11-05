41cce65c7fe9638b4153f95b4950b3720872a1a7
Ana Marija <47063623+AneAniks@users.noreply.github.com>
2019-11-05 23:31:47 +0100

    nonConsecutive TrialMidTerm

---
+++ nonConsecutive/nonConsecutive/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace nonConsecutive
+{
+    class Program
+    {
+        public static object FirstNonConsecutive(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; ++i)
+            {
+                if (arr[i] + 1 != arr[i + 1])
+                {
+                    return arr[i + 1];
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            string leng;
+            int lengNum;
+
+            Console.Write("Enter size of the array: ");
+
+            leng = Console.ReadLine();
+            lengNum = int.Parse(leng);
+
+            int[] input = new int[lengNum];
+
+            Console.WriteLine("Input elements in the array: ");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+
+                // ne znaev kako na drug nacin da go ispecatam
+                // ne mozev da go stornam vo variabla deka e od object type
+                Console.WriteLine("First non-Consecutive number is: " + FirstNonConsecutive(input)); 
+            }     
+        }
+    }
+}
