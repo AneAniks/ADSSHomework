9d3f9ae9b9ec8ed5032ab20c97c6d89b4d1c88b6
Ana Marija <47063623+AneAniks@users.noreply.github.com>
2019-11-05 12:24:00 +0100

    Grater than 10

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int number;
+
+            while (true)
+            {
+                Console.Write("Enter integer number: ");
+                input = Console.ReadLine();
+                number = int.Parse(input);
+
+                if (number >= 10)
+                {
+                    Console.WriteLine("Integer greater than 10 detected!");
+                    break;
+                }
+            }
+        }
+    }
+}
