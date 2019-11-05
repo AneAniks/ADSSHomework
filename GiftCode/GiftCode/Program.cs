d1be4ca767373b9cb07e8bdebbeb4991bca07be9
Ana Marija <47063623+AneAniks@users.noreply.github.com>
2019-11-05 23:53:55 +0100

    Gift Code

---
+++ GiftCode/GiftCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GiftCode
+{
+    class Program
+    {
+        public static string SortGiftCode(string giftCode)
+        {
+            char[] chars = new char[giftCode.Length];
+            string result = "";
+
+            for (int i = 0; i < giftCode.Length; i++)
+            {
+                chars[i] = giftCode[i];
+            }
+
+            Array.Sort(chars);
+
+            for (int i = 0; i < giftCode.Length; i++)
+            {
+                result += chars[i];
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.Write("Enter string: ");
+            input = Console.ReadLine();
+
+            Console.Write("Sorted string: ");
+            Console.WriteLine(SortGiftCode(input));
+        }
+    }
+}
